d2d44032e5d36be6e8aa15b961176c782e0c8007
alex chousmith <alex@ninthlink.com>
2015-05-22 13:34:50 -0700

    omg why is this so difficult push tracking script add

--- templates/smnlegacy-squeeze/header.php
+++ templates/smnlegacy-squeeze/header.php
@@ -7,7 +7,23 @@
 <?php if(function_exists('tha_html_before')) tha_html_before(); ?>
 <html class=" js no-touch svg inlinesvg svgclippaths no-ie8compat" <?php language_attributes(); ?>>
 <head>
-
+	
+	<?php
+		 if(is_page('savemarriage-2')){ ?>
+			 
+			 <!-- Start Visual Website Optimizer Asynchronous Code -->
+<script type='text/javascript'>
+var _vwo_code=(function(){
+var account_id=43506,
+settings_tolerance=2000,
+library_tolerance=2500,
+use_existing_jquery=false,
+// DO NOT EDIT BELOW THIS LINE
+f=false,d=document;return{use_existing_jquery:function(){return use_existing_jquery;},library_tolerance:function(){return library_tolerance;},finish:function(){if(!f){f=true;var a=d.getElementById('_vis_opt_path_hides');if(a)a.parentNode.removeChild(a);}},finished:function(){return f;},load:function(a){var b=d.createElement('script');b.src=a;b.type='text/javascript';b.innerText;b.onerror=function(){_vwo_code.finish();};d.getElementsByTagName('head')[0].appendChild(b);},init:function(){settings_timer=setTimeout('_vwo_code.finish()',settings_tolerance);this.load('//dev.visualwebsiteoptimizer.com/j.php?a='+account_id+'&u='+encodeURIComponent(d.URL)+'&r='+Math.random());var a=d.createElement('style'),b='body{opacity:0 !important;filter:alpha(opacity=0) !important;background:none !important;}',h=d.getElementsByTagName('head')[0];a.setAttribute('id','_vis_opt_path_hides');a.setAttribute('type','text/css');if(a.styleSheet)a.styleSheet.cssText=b;else a.appendChild(d.createTextNode(b));h.appendChild(a);return settings_timer;}};}());_vwo_settings_timer=_vwo_code.init();
+</script>
+<!-- End Visual Website Optimizer Asynchronous Code -->
+			 
+	<?php }  ?>
 	<?php if(function_exists('tha_head_top')) tha_head_top(); ?>
 
 	<meta charset="utf-8">
